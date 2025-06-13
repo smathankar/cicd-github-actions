a2a3d03aee506f92d932b000d01cae50726b85f5
smathankar <smathankar01@gmail.com>
2025-06-13 22:39:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-}
+#resource "aws_s3_bucket" "my_bucket" {
+#  bucket = var.bucket_name
+#  acl    = "private"
+#}
