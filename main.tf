c02144a5af7363a009a86b3fa8195933acd8746b
smathankar <smathankar01@gmail.com>
2025-06-16 12:33:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-#resource "aws_s3_bucket" "my_bucket" {
-#  bucket = var.bucket_name
-#  acl    = "private"
-#}
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
