d68dd45ea368e73678cbbb9ebb9cb5d7ae46214b
smathankar <smathankar01@gmail.com>
2025-06-16 12:52:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,10 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
   acl    = "private"
+  tags = {
+    Name        = var.bucket_name
+    Environment = "test"
+    Project     = "Terraform"
+    Owner       = "DevOpsTeam"
+  }
 }
